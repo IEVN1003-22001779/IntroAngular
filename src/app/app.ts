73ee4f15e64cd6a448f879363d11909dc8b39211
Luis Gael Cisneros Pérez <80247@alumnos.utleon.edu.mx>
2026-09-10 23:15:13 -0600

    Heroes 10/09/2026

---
+++ src/app/app.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: false,
+  styleUrl: './app.css',
+  templateUrl: './app.html',
+})
+export class App {
+  title:string='Luis Gael Cisneros Pérez'
+
+  duplicarnumero(a:number):number{
+    return a*2
+  }
+
+  pelicula={
+    titulo:'Spider-man',
+    fechaLanzamiento: new Date(),
+    precio:2353
+  }
+
+  
+}
